82d87cebedde92a518ce1b2c42c7659484d3ceff
Michael Lucci <lockejam@users.noreply.github.com>
2025-11-27 15:04:44 -0500

    Updated index for some spelling and formatting updates and main.js to fill in datetime attribute of time elements for present date designations

--- js/main.js
+++ js/main.js
@@ -7,5 +7,24 @@ document.addEventListener('DOMContentLoaded', () => {
   initSizeObserver();
   initBackToTop();
   scheduleExternalLinks();
-  
+
+  applyCurrentYearMonthToPresentDates();
 });
+
+/**
+ * Sets the datetime attribute of all <time> elements with the
+ * "class=datetime-present" to the current year and month in "YYYY-MM" format.
+ * 
+ */
+const applyCurrentYearMonthToPresentDates = () => {
+
+  // Format: "YYYY-MM" (e.g., "2024-06")
+  const currentYearMonth = new Date().toISOString().slice(0, 7);
+
+  /** @type {NOdeListOf<HTMLTimeElement} */
+  const elements = document.querySelectorAll('time.datetime-present');
+
+  elements.forEach((element) => {
+      element.dateTime = currentYearMonth;
+  });
+}
